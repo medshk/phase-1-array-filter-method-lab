f3e69ee9fe35b8c45f0a300791c99c56b0f0efde
med <mohammed.bennaceur98@gmail.com>
2023-09-02 20:58:41 +0100

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+const findMatching = (arr, searchedName) =>
+  arr.filter((name) => name.toUpperCase() === searchedName.toUpperCase());
+
+const fuzzyMatch = (arr, char) =>
+  arr.filter((name) => name[char.length - 1] === char[char.length - 1]);
+
+const matchName = (arr, searchedName) =>
+  arr.filter(({ name }) => name.toUpperCase() === searchedName.toUpperCase());
